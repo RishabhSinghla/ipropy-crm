ad38bceaa1de6fe85bbb6e4d027e104cdc7079eb
ipropy <ipropy@gmail.com>
2026-09-13 20:07:52 +0530

    fix hierarchy capability migration

--- packages/server/src/db/migrations/147_preserve_existing_manager_visibility.sql
+++ packages/server/src/db/migrations/147_preserve_existing_manager_visibility.sql
@@ -2,5 +2,5 @@
 -- that behaviour after hierarchy visibility became an explicit role setting;
 -- an administrator can now turn it off per role in Roles & Profiles.
 UPDATE ipy_profile
-SET capabilities = array_append(capabilities, 'records.view_lower_hierarchy')
-WHERE NOT ('records.view_lower_hierarchy' = ANY(capabilities));
+SET capabilities = capabilities || jsonb_build_array('records.view_lower_hierarchy')
+WHERE NOT capabilities @> '["records.view_lower_hierarchy"]'::jsonb;
